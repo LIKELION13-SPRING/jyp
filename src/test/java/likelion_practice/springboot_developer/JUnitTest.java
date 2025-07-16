f4bbf93c1781cdc2df2a196270bb56c9080d3803
Jiyoonpark0208 <ascii0208@naver.com>
2025-07-16 15:59:04 +0900

    by chapter4

--- src/test/java/likelion_practice/springboot_developer/JUnitTest.java
+++ src/test/java/likelion_practice/springboot_developer/JUnitTest.java
@@ -1,17 +1,18 @@
 package likelion_practice.springboot_developer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JUnitTest {
-    @DisplayName("Test")
+    @DisplayName("1+2는 3이댜")
     @Test
-    public void insertTest(){
+    public void junitTest(){
         int a=1;
         int b=2;
         int sum=3;
 
-        assertThat(a+b).isEqualTo(sum);
+        Assertions.assertEquals(sum,a+b);
     }
 }
