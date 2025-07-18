c7c304c46f15a2500e4b22a52291d5217e7d1e19
Jiyoonpark0208 <ascii0208@naver.com>
2025-07-18 15:34:23 +0900

    by chapter5

--- src/main/java/likelion_practice/springboot_developer/MemberRepository.java
+++ src/main/java/likelion_practice/springboot_developer/MemberRepository.java
@@ -3,6 +3,9 @@ package likelion_practice.springboot_developer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByName(String name);
 }
